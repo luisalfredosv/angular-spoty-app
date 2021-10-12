b309a9d2fa62c6b09a6e6e90a3204896b35869cd
luisalfredosv <luissarabia80@gmail.com>
2021-10-12 13:58:14 -0400

    [update] Componente MediaPlayer

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -7,7 +8,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
 
   mockCover: TrackModel = {
     cover: 'https://jenesaispop.com/wp-content/uploads/2009/09/guetta_onelove.jpg',
@@ -17,7 +18,23 @@ export class MediaPlayerComponent implements OnInit {
     _id: 1
   }
 
+  listObservers$ : Subscription[] = []
+
+  constructor( private multimediaServ: MultimediaService ){}
+
   ngOnInit(): void {
+    const observer1$: Subscription = this.multimediaServ.callback.subscribe(
+      (response: TrackModel) => {
+        console.log('Play ', response);
+
+      }
+    )
+
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach( u => u.unsubscribe())
   }
 
 }
