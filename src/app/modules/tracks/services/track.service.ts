562cdd125b21517f96cb5dc841ba6f3bea7b245b
luisalfredosv <luissarabia80@gmail.com>
2021-10-13 05:07:37 -0400

    [create] Servicio de Track

---
+++ src/app/modules/tracks/services/track.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { Observable, of } from 'rxjs';
+import * as dataRaw from "../../../data/tracks.json";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrackService {
+
+  dataTracksTrending$ :Observable<TrackModel[]> = of([])
+  dataTracksRandom$:Observable<TrackModel[]> = of([])
+
+  constructor() {
+    const { data }: any = (dataRaw as any).default;
+    this.dataTracksTrending$ = of(data)
+    this.dataTracksRandom$ = of(data)
+  }
+
+}
