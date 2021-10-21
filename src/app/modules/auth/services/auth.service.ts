ae04c8f49485fc142161198854687303810c68a7
luisalfredosv <luissarabia80@gmail.com>
2021-10-21 16:39:11 -0400

    [update] Conectandose a la API

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly URL = environment.api;
+  constructor(
+    private http: HttpClient,
+    private cookie: CookieService) {}
 
-  constructor() { }
-
-  sendCredentials( email:string, password: string ):void{
-    console.log('OK', { email, password })
+  sendCredentials(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.URL}/auth/login`, {
+      email,
+      password,
+    }).pipe(
+      tap((response: any ) => {
+        const { accessToken } = response
+        this.cookie.set('accessToken', accessToken,  4, '/')
+      })
+    )
   }
 }
