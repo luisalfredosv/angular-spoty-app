4a252354c330a77345b9a86069d533726dba2a8b
luisalfredosv <luissarabia80@gmail.com>
2021-10-21 16:44:19 -0400

    [update] Servicio Multimedia

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,11 +1,108 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MultimediaService {
+  callback: EventEmitter<any> = new EventEmitter<any>();
+  public audio!: HTMLAudioElement;
 
-  callback: EventEmitter<any> = new EventEmitter<any>()
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject(
+    '-00:00'
+  );
 
-  constructor() { }
+  public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused');
+  public playerPercentage$: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  constructor() {
+    this.audio = new Audio();
+    this.trackInfo$.subscribe((response) => {
+      response && this.setAudio(response);
+    });
+
+    this.listenAllEvents();
+  }
+
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);
+    this.audio.addEventListener('playing', this.setPlayerStatus, false);
+    this.audio.addEventListener('play', this.setPlayerStatus, false);
+    this.audio.addEventListener('pause', this.setPlayerStatus, false);
+    this.audio.addEventListener('ended', this.setPlayerStatus, false);
+  }
+
+  private calculateTime = () => {
+    const { duration, currentTime } = this.audio;
+    this.setTimeElapsed(currentTime);
+    this.setTimeRemaining(currentTime, duration);
+    this.setPercentage(currentTime, duration);
+  };
+
+  private setTimeElapsed(currentTime: number): void {
+    const { displaySeconds, displayMinutes} = this.returnTime(currentTime);
+
+    const displayFormat = `${displayMinutes}:${displaySeconds}`;
+    this.timeElapsed$.next(displayFormat);
+  }
+
+  private setPercentage(currentTime: number, duration: number): void {
+    const percentage = (currentTime * 100) / duration;
+    this.playerPercentage$.next(percentage);
+  }
+
+  private setTimeRemaining(currentTime: number, duration: number) {
+    const timeLeft = duration - currentTime;
+    const { displaySeconds, displayMinutes } = this.returnTime(timeLeft);
+
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`;
+    this.timeRemaining$.next(displayFormat);
+  }
+
+
+  private returnTime(time: number){
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor((time / 60) % 60);
+    const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return {
+      displaySeconds,
+      displayMinutes
+    }
+  }
+
+  private setPlayerStatus = (state: any) => {
+    switch (state.type) {
+      case 'play':
+        this.playerStatus$.next('play');
+        break;
+      case 'playing':
+        this.playerStatus$.next('playing');
+        break;
+      case 'ended':
+        this.playerStatus$.next('ended');
+        break;
+      default:
+        this.playerStatus$.next('paused');
+        break;
+    }
+  };
+
+  public setAudio(track: TrackModel): void {
+    this.audio.src = track.url;
+    this.audio.play();
+  }
+
+  public togglePlayer(): void {
+    this.audio.paused ? this.audio.play() : this.audio.pause();
+  }
+
+  public seekAudio(percentage: number): void {
+    const { duration } = this.audio;
+    const percentageToSecond = (percentage * duration) / 100;
+    this.audio.currentTime = percentageToSecond;
+  }
 }
